d85d19a5e96476f0247284cfea6817ac60c56f42
Duane Wilson <duanewilsonsf@gmail.com>
2014-01-03 10:37:13 -0800

    Analytics.js
    Google Analytics.js Integration into <head></head>
    
    Can be deleted if using an existing ga.js account, this one should be
    used for new tracking accounts.
    
    https://developers.google.com/analytics/devguides/collection/analyticsjs
    /
    
    “The analytics.js JavaScript snippet is a new way to measure how users
    interact with your website. It is similar to the previous tracking
    code, ga.js, but offers more flexibility for developers to customize
    their implementations.”

--- header.php
+++ header.php
@@ -56,6 +56,15 @@
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/backstretch.js"></script>
 	<!--[if lte IE 7]><script src="<?php bloginfo('template_url'); ?>/icomoon/lte-ie7.js"></script><![endif]-->
+
+<!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-XXXX-Y', 'auto');
+ga('send', 'pageview');
+</script>
+<!-- End Google Analytics -->
 </head>
 
 <body <?php body_class(); ?>>
